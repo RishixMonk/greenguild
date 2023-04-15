9ed8c73fd5100889c80105da94715cf753f50044
M.B.SAI ADITYA <mbsaiaditya@gmail.com>
2023-04-16 01:39:45 +0530

    submit api changed

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from api.models import Question
 from django.http import HttpResponse,StreamingHttpResponse,JsonResponse, request
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-import math
+import math, json
 # Create your views here.
 
 def isauth(request):
@@ -54,7 +54,7 @@ def submit_data(request):
     # if request.user.is_authenticated and request.method == 'POST':
     if request.method == 'POST':
         try:
-            form_data = dict(request.POST)
+            form_data = json.loads(request.body)
             question_data_map = {}
             category_data_map = {}
             instances = list(Question.objects.all())
@@ -65,8 +65,8 @@ def submit_data(request):
                 category_data_map[str(question.id)] = question.category
             total_carbon_count = 0
             for key, val in form_data.items():
-                categories[category_data_map[key]]+=question_data_map[key] * float(val[0])
-                total_carbon_count+= question_data_map[key] * float(val[0])
+                categories[category_data_map[key]]+=question_data_map[key] * float(val)
+                total_carbon_count+= question_data_map[key] * float(val)
             total_score = int(total_carbon_count/10)
             commute_score = int(categories['Commute']/10)
             household_score = int(categories['Household']/10)
