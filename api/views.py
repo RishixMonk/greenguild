bef26407cc316f3678a350cbc081382acf6054df
M.B.SAI ADITYA <mbsaiaditya@gmail.com>
2023-04-15 18:16:45 +0530

    added percentage params

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from api.models import Question
 from django.http import HttpResponse,StreamingHttpResponse,JsonResponse, request
+from api.definitions import AVG_CONSUMPTION_COMMUTE, AVG_CONSUMPTION_FOOD, AVG_CONSUMPTION_HOUSEHOLD
 # Create your views here.
 def questionwithcategory(request,ctg):
     try:
@@ -31,6 +32,9 @@ def submit_data(request):
             for key, val in form_data.items():
                 categories[category_data_map[key]]+=question_data_map[key] * float(val[0])
                 total_carbon_count+= question_data_map[key] * float(val[0])
+            categories['percentage_wrt_avg_Commute'] = ((categories['Commute']-AVG_CONSUMPTION_COMMUTE)/AVG_CONSUMPTION_COMMUTE)*100
+            categories['percentage_wrt_avg_Household'] = ((categories['Household']-AVG_CONSUMPTION_HOUSEHOLD)/AVG_CONSUMPTION_COMMUTE)*100
+            categories['percentage_wrt_avg_Food'] = ((categories['Food']-AVG_CONSUMPTION_FOOD)/AVG_CONSUMPTION_COMMUTE)*100
             return JsonResponse({'total_carbon_count': total_carbon_count,"categories":categories})
         except Exception as e:
             return JsonResponse({'error': "Key Error"})
